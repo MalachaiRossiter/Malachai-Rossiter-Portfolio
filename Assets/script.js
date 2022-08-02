3c73b72ff6667f18abf2c70e3a77250297814b18
Malachai Rossiter <malachai.rossiter@gmail.com>
2022-08-01 21:18:20 -0400

    got button to work

--- Assets/script.js
+++ Assets/script.js
@@ -4,16 +4,19 @@ const cursor = document.querySelector(".cursor");
 document.addEventListener("mousemove", e => {
     cursor.setAttribute("style", "top: " +(e.pageY-3)+ "px; left: "+(e.pageX-3)+"px;");
 })
-// Having the trailing Cursor Follow you
+// Having the trailing Cursor Follow you //Changing the follow cursor on hover by hand
 const cursorFollow = document.querySelector(".cursor-follow");
 document.addEventListener("mousemove", e => {
+    console.log(e.target)
     cursorFollow.setAttribute("style", "top: " +(e.pageY-10.5)+ "px; left: "+(e.pageX-10.5)+"px;");
+
+    if (e.target.classList.contains('linkitem')) {
+        cursorFollow.classList.add('red');
+        cursorFollow.setAttribute("style", "top: " +(e.pageY-21)+ "px; left: "+(e.pageX-21)+"px;");
+    } else {
+        cursorFollow.classList.remove('red');
+    }
 })
-//Changing the follow cursor on hover
-
-
-
-
 
 //Copy to ClipBoard
 function copyClipboard(){
