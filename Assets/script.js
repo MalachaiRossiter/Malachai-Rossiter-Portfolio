fa4b9da88c7eab5babefa2b83f18c76bcab3c602
Malachai Rossiter <malachai.rossiter@gmail.com>
2022-08-09 16:28:47 -0400

    took out uneeded code

--- Assets/script.js
+++ Assets/script.js
@@ -37,24 +37,3 @@ document.querySelectorAll(".navlink").forEach(n => n.addEventListener("click", (
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
-
-//fade items in
-
-window.addEventListener("scroll", reveal);
-
-function reveal(){
-    var reveals = document.querySelectorAll(".reveal");
-
-    for (var i = 0; i < reveals.length; i++){
-        var windowheight = window.innerHeight;
-        var revealtop = reveals[i].getBoundingClientRect().top;
-        var revealpoint = 150;
-    }
-
-    if(revealtop < windowheight - revealpoint){
-        reveals[i].classList.add("activefade");
-    }
-    else{
-        reveals[i].classList.remove("activefade");
-    }
-}
